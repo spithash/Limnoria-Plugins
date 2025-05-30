05d417c62cdf4b5f035c9452df20dcd227c52414
spithash <spithash@tuta.io>
2025-05-30 13:38:40 +0300

    changed default cutoff time for commits to 6 minutes

--- GitPulse/plugin.py
+++ GitPulse/plugin.py
@@ -319,7 +319,7 @@ class GitPulse(callbacks.Plugin):
             return
 
         now = datetime.now(timezone.utc)
-        cutoff = now - timedelta(minutes=5)
+        cutoff = now - timedelta(minutes=6)
 
         for commit in reversed(commits):
             try:

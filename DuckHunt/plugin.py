d6b9a40befd409a397971d1966328e0c8068ec2e
spithash <spithash@tuta.io>
2025-10-10 11:08:23 +0300

    changed description

--- DuckHunt/plugin.py
+++ DuckHunt/plugin.py
@@ -44,7 +44,7 @@ class DuckHunt(callbacks.Plugin):
     """
     A DuckHunt game for supybot. Use the "starthunt" command to start a game.
     The bot will randomly launch ducks. Whenever a duck is launched, the first
-    person to use the "bang" command wins a point. Using the "bang" command
+    person to use the "bang" command wins a point. Same goes for "bef" which is befriending the ducks. Using the "bang" and the "bef" command
     when there is no duck launched costs a point.
     """
 
